03fabb3964c170b4c6d394f3c0fde08d598cfbb7
Erik Ostrom <erik@echographia.com>
2011-06-20 14:45:58 -0500

    Indentation fix.

--- test/test_tumblr_tags.rb
+++ test/test_tumblr_tags.rb
@@ -8,32 +8,32 @@ describe 'TumblrTags' do
   def app; Sinatra::Application; end
   def response_body; last_response.body; end
 
-    describe '/tags' do
-      before do
-        @blog = mock(Blog).find_or_initialize_by_name('erikostrom').mock!
-        @blog.tags do
-          TagCollection.new [
-            Tag.new(:name => 'few', :count => 3),
-            Tag.new(:name => 'many things', :count => 15)
-          ]
-        end
-
-        get '/tags', :blog => 'erikostrom'
+  describe '/tags' do
+    before do
+      @blog = mock(Blog).find_or_initialize_by_name('erikostrom').mock!
+      @blog.tags do
+        TagCollection.new [
+          Tag.new(:name => 'few', :count => 3),
+          Tag.new(:name => 'many things', :count => 15)
+        ]
       end
 
-      it 'renders a list of tags' do
-        assert last_response.ok?, last_response.errors
+      get '/tags', :blog => 'erikostrom'
+    end
 
-        assert_have_selector 'ul#tumblr_tags'
-        assert_have_selector 'ul#tumblr_tags li', :count => 2
-#         assert_have_selector('ul#tumblr_tags li:first-child a',
-#           :href => 'http://erikostrom.tumblr.com/tagged/many%20things')
-        assert_have_selector('ul#tumblr_tags li:first-child .tag',
-          :content => 'many things')
-        assert_have_selector('ul#tumblr_tags li:first-child .count',
-          :content => '15')
-      end
+    it 'renders a list of tags' do
+      assert last_response.ok?, last_response.errors
+
+      assert_have_selector 'ul#tumblr_tags'
+      assert_have_selector 'ul#tumblr_tags li', :count => 2
+      #         assert_have_selector('ul#tumblr_tags li:first-child a',
+      #           :href => 'http://erikostrom.tumblr.com/tagged/many%20things')
+      assert_have_selector('ul#tumblr_tags li:first-child .tag',
+        :content => 'many things')
+      assert_have_selector('ul#tumblr_tags li:first-child .count',
+        :content => '15')
     end
   end
+end
 
 
